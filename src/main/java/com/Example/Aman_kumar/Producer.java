0d584b4da9565e263ed73898f17cde86e1aa8990
kumar113 <aman.kumar@knoldus.com>
2022-01-10 21:24:57 +0530

    initial commit

---
+++ src/main/java/com/Example/Aman_kumar/Producer.java
@@ -0,0 +1,28 @@
+package com.Example.Aman_kumar;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "com.example.Aman_kumar.Serialization");
+
+        KafkaProducer<String, User> kafkaProducer = new KafkaProducer<>(properties);
+
+        try {
+            for (int i = 1; i <= 10; i++) {
+                User user = new User(i,"Aman",21,"Btech");
+                kafkaProducer.send(new ProducerRecord("mytopic2", String.valueOf(user.getId()), user));
+                //System.out.println(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            kafkaProducer.close();
+        }
+    }
+}
